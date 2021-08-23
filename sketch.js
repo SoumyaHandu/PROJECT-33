ed6da4db97d6596ed776d28d87e54406f3a09af6
SoumyaHandu <75081856+SoumyaHandu@users.noreply.github.com>
2021-08-23 19:19:53 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,10 @@
 var snowflake,snowflakeImg
+var background, bgImg
 
 function preload() {
 
-  snowflakeImg = loadImage("snow4.webp""snow1.jpg""snow2.jpg","snow3.jpg","snow4.webp","snow.webp");
+  snowflakeImg = loadImage("snow4.webp", "snow5.webp");
+  bgImg = loadImage("snow1.jpg","snow2.jpg", "snow3.jpg");
 }
 
 function setup() {
@@ -11,9 +13,19 @@ createCanvas(800,400);
 }
 
 function draw() {
-  background("blue");  
+  background(bgImg);  
   drawSprites();
   createSnowflake();
+}
+
+function createSnowyBackground(){
+  if(frameCount%20===0){
+  background = createSprite(random(0,800), 0, 50, 50);
+  background.velocityX = -2;
+  background.velocityY = 3;
+  background.addImage(bgImg);
+  background.scale = 2;
+  }
 }
 
 function createSnowflake(){
